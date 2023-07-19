e7a641cf393757cbb97d562f4802e7e33de2ccb0
shantdashjian <shantgdashjian@gmail.com>
2023-07-19 15:11:08 +0400

    Modify online message

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ if (require.main === module) {
     // When this script is run as the entry point, starts the HTTP server.
     //
     app.listen(PORT, () => {
-        console.log(`Microservice online!`);
+        console.log(`Microservice online.`);
     });
 }
 else {
